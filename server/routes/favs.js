7c43db6670655c61a49498aba6ec99e3c9dd1b16
tommyrharper <thomasrobertharper@gmail.com>
2021-02-05 04:09:50 +0000

    Add favs post route

--- server/routes/favs.js
+++ server/routes/favs.js
@@ -5,7 +5,9 @@ const fileController = require('../controllers/fileController');
 const router = express.Router();
 
 // ADD STORE FAVORITE ROUTE HANDLER HERE
-
+router.post('/:id', fileController.getFavs, fileController.addFav, (req, res) => {
+  return res.status(200).json(res.locals);
+});
 
 // ADD REMOVE FAVORITE ROUTE HANDLER HERE
 
