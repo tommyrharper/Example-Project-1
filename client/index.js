f8633cb7932986319742db25ce689860944a2940
Paulo Choi <paulochoi@Paulos-MBP.fios-router.home>
2021-03-25 23:03:38 -0400

    Frontend commit for UL in app

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './ultimate-logger/ultimate-logger.js';
 
 render(
   <BrowserRouter>
