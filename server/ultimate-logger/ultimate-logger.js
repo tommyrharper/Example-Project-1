c0bb805d66251f0f3c7f2ec89e41d33a3ea1663b
Paulo Choi <paulochoi@Paulos-MBP.fios-router.home>
2021-03-27 14:38:27 -0400

    run prettier and remove console.history functionality

--- server/ultimate-logger/ultimate-logger.js
+++ server/ultimate-logger/ultimate-logger.js
@@ -46,13 +46,15 @@ console._collect = function (type, args) {
       Accept: 'application/json',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ type: type, timestamp: time, arguments: args, stack: stack }),
-  }).then().catch(() => console._error('Connection refused to the Ultimate Logger server'))
-
-  console.history.push({
-    type: type,
-    timestamp: time,
-    arguments: args,
-    stack: stack,
-  });
+    body: JSON.stringify({
+      type: type,
+      timestamp: time,
+      arguments: args,
+      stack: stack,
+    }),
+  })
+    .then()
+    .catch(() =>
+      console._error('Connection refused to the Ultimate Logger Server')
+    );
 };
